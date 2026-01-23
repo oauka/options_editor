d2f26ff4a3ab7cf41fac89103a00c951e9eec5b6
oauka <77268885+oauka@users.noreply.github.com>
2026-01-24 00:00:30 +0900

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ProductOptionsEditor from './ProductOptionsEditor';
+
+function App() {
+  return (
+    <div className="App">
+      <ProductOptionsEditor />
+    </div>
+  );
+}
+
+export default App;
